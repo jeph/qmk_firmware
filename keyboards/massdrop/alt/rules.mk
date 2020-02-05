346ce761a58569e97c6f91ab2f0a391e5e1be628
Jeph <jephliu@jeph.io>
2020-02-05 16:33:42 -0500

    Disable leds for now

--- keyboards/massdrop/alt/rules.mk
+++ keyboards/massdrop/alt/rules.mk
@@ -31,8 +31,3 @@ HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 VIRTSER_ENABLE = no         # USB Serial Driver
 RAW_ENABLE = no             # Raw device
 AUTO_SHIFT_ENABLE = no      # Auto Shift
-
-# Custom RGB matrix handling
-RGB_MATRIX_ENABLE = custom
-
-LAYOUTS = 65_ansi_blocker
